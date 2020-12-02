e6fd15a6f3ad2e605ea831340a70e8a8e41336b9
mag389 <mag389@cornell.edu>
2020-12-02 18:42:54 +0000

    concatenate function to avoid conditionals

--- math/0x00-linear_algebra/13-cats_got_your_tongue.py
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -5,6 +5,4 @@ import numpy as np
 
 def np_cat(mat1, mat2, axis=0):
     """concatenates matrices, axis=0 for vertical, 1 for horizontal"""
-    if axis == 0:
-        return np.vstack((mat1, mat2))
-    return np.hstack((mat1, mat2))
+    return np.concatenate((mat1, mat2), axis)
