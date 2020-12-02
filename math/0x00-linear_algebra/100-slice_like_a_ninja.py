9239af5be7b8e7cad69bb7c7bfe25527ea3970da
mag389 <mag389@cornell.edu>
2020-12-02 20:10:34 +0000

    code cleanup and import

--- math/0x00-linear_algebra/100-slice_like_a_ninja.py
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python3
 """take a multidmensional slice of a matrix """
+import numpy as np
 
 
 def np_slice(matrix, axes={}):
     """slice matrix according to slicer dict"""
     slicer = [slice(None) for x in range(len(matrix.shape))]
-    # #print(slicer)
     for key, value in axes.items():
-        # #args = [None, None, None]
-        # #for elem in range(len(value)):
-        # #    args[elem] = value[elem]
-        # #print(args)
         slicer[key] = slice(*value)
     return matrix[slicer]
