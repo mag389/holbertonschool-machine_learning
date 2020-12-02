b244b17686961d790fcc6eee99c7b7fa2194562e
mag389 <mag389@cornell.edu>
2020-12-02 17:58:15 +0000

    changing bounds to go to i instead of a static value

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -9,6 +9,6 @@ def add_matrices2D(mat1, mat2):
         return None
     newmat = [[0 for x in range(len(mat1))] for y in range(len(mat1[0]))]
     for i in range(len(mat1)):
-        for j in range(len(mat1[0])):
+        for j in range(len(mat1[i])):
             newmat[i][j] = mat1[i][j] + mat2[i][j]
     return newmat
