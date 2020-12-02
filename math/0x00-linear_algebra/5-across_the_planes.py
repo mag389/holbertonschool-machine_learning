949b43df0c9e94cf5db51fb255c3c93141d3bc6d
mag389 <mag389@cornell.edu>
2020-12-02 18:03:07 +0000

    more thorough shape check

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -7,8 +7,19 @@ def add_matrices2D(mat1, mat2):
     """
     if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         return None
+    if matrix_shape(mat1) != matrix_shape(mat2):
+        return None
     newmat = [[0 for x in range(len(mat1))] for y in range(len(mat1[0]))]
     for i in range(len(mat1)):
         for j in range(len(mat1[i])):
             newmat[i][j] = mat1[i][j] + mat2[i][j]
     return newmat
+
+
+def matrix_shape(matrix):
+    """ returns the matrix shape of a given matrix """
+    if len(matrix) is 0:
+        return [0]
+    if type(matrix[0]) is list:
+        return [len(matrix)] + matrix_shape(matrix[0])
+    return [len(matrix)]
