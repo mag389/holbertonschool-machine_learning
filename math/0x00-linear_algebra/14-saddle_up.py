b84569f12fd131ed918b07356d01ad4ca639db82
mag389 <mag389@cornell.edu>
2020-12-02 18:55:17 +0000

    added an import line

--- math/0x00-linear_algebra/14-saddle_up.py
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """ matrix multiplier """
+import numpy as np
 
 
 def np_matmul(mat1, mat2):
     """returns the product of two matrices"""
-    return mat1.matmul(mat2)
+    return mat1 @ mat2
