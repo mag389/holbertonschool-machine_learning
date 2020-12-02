5b61920a2f62c593449e70d65e2f34923d52218f
mag389 <mag389@cornell.edu>
2020-12-02 18:47:38 +0000

    dot product multiplication with numpy

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" matrix multiplier """
+
+
+def np_matmul(mat1, mat2):
+    """returns the product of two matrices"""
+    return mat1 @ mat2
+    """alternate: mat1.dot(mat2)"""
