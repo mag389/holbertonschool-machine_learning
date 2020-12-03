dd5e1788acd87933b6077ca57f4ce87604a2230a
mag389 <mag389@cornell.edu>
2020-12-03 20:20:24 +0000

    deep copy instead of shallow

--- math/0x00-linear_algebra/102-squashed_like_sardines.py
+++ math/0x00-linear_algebra/102-squashed_like_sardines.py
@@ -6,25 +6,29 @@ def cat_matrices(mat1, mat2, axis=0):
     """concatenate two matrices along the given axis """
     if axis == 0:
         # if type(mat1[0]) != type(mat2[0]):
-        #    return None
         # deprecated method
         if not isinstance(mat1[0], type(mat2[0])):
             return None
         if len(matrix_shape(mat1)) != len(matrix_shape(mat2)):
             return None
         newmat = [0 for x in range(len(mat1) + len(mat2))]
-        for i in range(len(mat1)):
-            newmat[i] = mat1[i]
-        for i in range(len(mat2)):
-            newmat[i + len(mat1)] = mat2[i]
+        if not isinstance(newmat[0], list):
+            for i in range(len(mat1)):
+                newmat[i] = mat1[i]
+            for i in range(len(mat2)):
+                newmat[i + len(mat1)] = mat2[i]
+        else:
+            for i in range(len(mat1)):
+                newmat[i] = mat1[i].deepcopy()
+            for i in range(len(mat2)):
+                newmat[i + len(mat1)] = mat2[i].deepcopy()
         return newmat
+
     if axis >= 1:
         if len(matrix_shape(mat1)) <= axis or len(matrix_shape(mat2)) <= axis:
             return None
         newmat = [0 for x in range(len(mat1))]
         for i in range(len(mat1)):
-            # if type(mat1[i]) != type(mat2[i]):
-            #     return None
             if not isinstance(mat1[i], type(mat2[i])):
                 return None
             newmat[i] = cat_matrices(mat1[i], mat2[i], axis - 1)
@@ -36,6 +40,8 @@ def cat_matrices(mat1, mat2, axis=0):
 
 def matrix_shape(matrix):
     """ returns the matrix shape of a given matrix """
+    if not matrix:
+        return None
     if len(matrix) is 0:
         return [0]
     if type(matrix[0]) is list:
