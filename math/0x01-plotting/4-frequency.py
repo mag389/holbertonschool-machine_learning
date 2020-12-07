9de0cb4eb04d31ad83ab51cf2adeeb5bb27a7e3d
mag389 <mag389@cornell.edu>
2020-12-07 17:38:45 +0000

    basic histogram

---
+++ math/0x01-plotting/4-frequency.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(5)
+student_grades = np.random.normal(68, 15, 50)
+
+# my code here
+
+plt.xlabel("Grades")
+plt.ylabel("Number of Students")
+plt.title("Project A")
+plt.xlim(0, 100)
+plt.ylim(0, 30)
+plt.hist(student_grades, range=(0, 100), bins=10, edgecolor='black')
+plt.show()
