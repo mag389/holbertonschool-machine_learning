fd946ceff05f74f0498f1b4390ab772cf0dcbf95
mag389 <mag389@cornell.edu>
2020-12-10 19:53:02 +0000

    trying different method for zero removal

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -18,7 +18,7 @@ def poly_integral(poly, C=0):
     retval[0] = C
     for i in range(len(retval)):
         if retval[len(retval) - i - 1] == 0:
-            retval.pop(len(retval) - i)
+            retval.pop(len(retval) - i - 1)
         else:
             break
     return retval
