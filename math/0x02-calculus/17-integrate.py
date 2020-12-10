523254d2eef5fe54f6b59c5b01cb472d545b7aaf
mag389 <mag389@cornell.edu>
2020-12-10 20:03:23 +0000

    various bug serch changes such as type checking for floats and ints

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -4,7 +4,9 @@
 
 def poly_integral(poly, C=0):
     """integrate the polynomial poly, return list of co-efficients"""
-    if not poly or not isinstance(C, int):
+    if not poly:
+        return None
+    if not isinstance(C, int) and not isinstance(C, float):
         return None
     if not isinstance(poly, list):
         return None
@@ -12,10 +14,14 @@ def poly_integral(poly, C=0):
         return None
     retval = [0 for x in range(len(poly) + 1)]
     for i in range(len(poly)):
+        if type(poly[i]) is not int and type(poly[i]) is not float:
+            return None
         retval[i + 1] = poly[i] / (i + 1)
         if retval[i + 1] == int(retval[i + 1]):
             retval[i + 1] = int(retval[i + 1])
     retval[0] = C
+    if int(C) == C:
+        retval[0] == int(C)
     for i in range(len(retval)):
         if retval[len(retval) - i - 1] == 0:
             retval.pop(len(retval) - i - 1)
