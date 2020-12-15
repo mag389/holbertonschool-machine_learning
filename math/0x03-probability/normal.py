49efbd1f3cea1b72120898edc08fb41546cbe7b1
mag389 <mag389@cornell.edu>
2020-12-15 22:34:24 +0000

    tried a lot of things can't get cdf past ~12 decimal accuracy to the example output

--- math/0x03-probability/normal.py
+++ math/0x03-probability/normal.py
@@ -42,3 +42,22 @@ class Normal:
         expo = -.5 * ((x - self.mean) / self.stddev) ** 2
         pdfval = coeff * e ** (expo)
         return pdfval
+
+    @staticmethod
+    def erf(x):
+        """return the erf for x """
+        pi = 3.1415926536
+        coeff = 2 / (pi ** .5)
+        series = x - x ** 3 / 3 + x ** 5 / 10 - x ** 7 / 42 + x ** 9 / 216
+        return float(coeff * series)
+
+    def cdf(self, x):
+        """the cdf for the normal distribution object """
+        pi = 3.1415926536
+        e = 2.7182818285
+        erfval = (x - self.mean) / (self.stddev * 2 ** .5)
+        y = erfval
+        series = (1 + (y - y ** 3 / 3 + y ** 5 / 10 - y ** 7 / 42
+                       + y ** 9 / 216) * 2 / (pi ** .5)) / 2
+        cdfval = (.5 + series)
+        return series
