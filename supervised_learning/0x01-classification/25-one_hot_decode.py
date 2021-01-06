ef05a827facb479dfe7636f6f698d0e9e4f70274
mag389 <mag389@cornell.edu>
2021-01-06 18:35:24 +0000

    shape check

--- supervised_learning/0x01-classification/25-one_hot_decode.py
+++ supervised_learning/0x01-classification/25-one_hot_decode.py
@@ -6,6 +6,9 @@ import numpy as np
 def one_hot_decode(one_hot):
     """ decodes the one hot matrix """
     try:
+        decoded = np.argmax(one_hot, axis=0)
+        if decoded.shape != ((one_hot.shape[1],)):
+            return None
         return np.argmax(one_hot, axis=0)
     except Exception:
         return None
