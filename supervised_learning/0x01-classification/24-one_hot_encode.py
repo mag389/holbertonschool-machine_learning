0592c84db55421b39ff5db9c04c3c216dfe4bfab
mag389 <mag389@cornell.edu>
2021-01-06 18:20:03 +0000

    added shape check

--- supervised_learning/0x01-classification/24-one_hot_encode.py
+++ supervised_learning/0x01-classification/24-one_hot_encode.py
@@ -8,6 +8,9 @@ import numpy as np
 def one_hot_encode(Y, classes):
     """one hot encode the data Y into classes number of classes """
     try:
+        encoded = np.eye(classes)[Y].T
+        if encoded.shape != (classes, Y.shape[0]):
+            return None
         return np.eye(classes)[Y].T
     except Exception:
         return None
