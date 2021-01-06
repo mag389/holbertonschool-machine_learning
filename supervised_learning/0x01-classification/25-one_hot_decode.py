973adc2fb509f24d3a90d58e560f06afd2fdbbee
mag389 <mag389@cornell.edu>
2021-01-06 18:33:06 +0000

    updated decoder with try except

--- supervised_learning/0x01-classification/25-one_hot_decode.py
+++ supervised_learning/0x01-classification/25-one_hot_decode.py
@@ -5,4 +5,7 @@ import numpy as np
 
 def one_hot_decode(one_hot):
     """ decodes the one hot matrix """
-    return np.argmax(one_hot, axis=0)
+    try:
+        return np.argmax(one_hot, axis=0)
+    except Exception:
+        return None
