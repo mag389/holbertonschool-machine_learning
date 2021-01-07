93d7da8060897f5830f69ebdcf8a9e5cb368efe2
mag389 <mag389@cornell.edu>
2021-01-07 21:45:10 +0000

    same file with explanation

--- supervised_learning/0x02-tensorflow/5-create_train_op.py
+++ supervised_learning/0x02-tensorflow/5-create_train_op.py
@@ -11,6 +11,20 @@ def create_train_op(loss, alpha):
         alpha is the learning rate
         Returns: an operation that trains the network using gradient descent
     """
+    """
+       the reason i use two functions instead of just minimze is because
+       i was getting a weird error, it was not producing output:
+       and saying: ValueError: No gradients provided for any variable
+       i tried using different functions here before looking it up
+       https://github.com/tensorflow/tensorflow/issues/42038
+       according to that it was probably an issue with data being passed
+       so i kept changing previous files. it turned out to be in 4-calc. file
+       i changed the args there from (y_pred, y) to (y, y_pred) and it worked
+       the corss entropy f'n was unclear which one went first and the checker
+       accepted my initial wrong guess.
+       this file actually worked with checker before it worked on my VM.
+       potentially a version issue on my machine
+    """
     gdo = tf.train.GradientDescentOptimizer(alpha)
     grads = gdo.compute_gradients(loss)
     op = gdo.apply_gradients(grads)
