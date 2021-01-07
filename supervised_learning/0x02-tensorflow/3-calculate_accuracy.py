d6d305cf43a4e26728a5c64bd3458ff31e3c2ae2
mag389 <mag389@cornell.edu>
2021-01-07 20:49:34 +0000

    accuracy function

---
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" accuracy function file """
+
+
+import tensorflow as tf
+
+
+def calculate_accuracy(y, y_pred):
+    """ calculates accuracy of a prediction
+        Return: the accuracy as tensor("Mean:0", shape=(), dtype=float32)
+            thattensor is the decimal accuracy of the prediction
+    """
+    # print(y)
+    # print(y_pred)
+    y1 = tf.argmax(y, 1)
+    yp1 = tf.argmax(y_pred, 1)
+
+    print("-" * 50)
+    print(y1)
+    print(yp1)
+    print("-" * 50)
+
+    err = tf.cast(y1 - yp1, tf.float32)
+    acc = tf.reduce_mean(err)
+    print(acc)
+    return acc
