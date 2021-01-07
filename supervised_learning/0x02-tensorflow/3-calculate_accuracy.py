bdd59c6b930c99196633cadcb1ceee3bb177e7cc
mag389 <mag389@cornell.edu>
2021-01-07 21:03:41 +0000

    found an alternative method

--- supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -15,12 +15,19 @@ def calculate_accuracy(y, y_pred):
     y1 = tf.argmax(y, 1)
     yp1 = tf.argmax(y_pred, 1)
 
-    print("-" * 50)
-    print(y1)
-    print(yp1)
-    print("-" * 50)
+    # print("-" * 50)
+    # print(y1)
+    # print(yp1)
+    # print("-" * 50)
 
-    err = tf.cast(y1 - yp1, tf.float32)
-    acc = tf.reduce_mean(err)
-    print(acc)
+    # err = tf.cast(y1 - yp1, tf.float32)
+    # acc = tf.reduce_mean(err)
+    # print(acc)
+
+    # https://stackoverflow.com/questions/42607930/
+    # how-to-compute-accuracy-of-cnn-in-tensorflow
+    # no space in url
+
+    equality = tf.math.equal(yp1, y1)
+    acc = tf.math.reduce_mean(tf.cast(equality, tf.float32))
     return acc
