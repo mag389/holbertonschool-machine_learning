a6cfa986e8a4f4ed1db2a4ec4695ada935f69ad4
mag389 <mag389@cornell.edu>
2021-01-20 18:06:40 +0000

    added the import lines

--- supervised_learning/0x04-error_analysis/4-f1_score.py
+++ supervised_learning/0x04-error_analysis/4-f1_score.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """ f1 calculation """
 import numpy as np
+sensitivity = __import__('1-sensitivity').sensitivity
+precision = __import__('2-precision').precision
 
 
 def f1_score(confusion):
