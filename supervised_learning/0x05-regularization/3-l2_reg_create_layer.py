c9f5ed79d69e526c3253d3f2e021e07ebcae77a7
mag389 <mag389@cornell.edu>
2021-01-22 19:18:07 +0000

    pycodestyle: shortened lines

--- supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
+++ supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
@@ -11,8 +11,10 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
         lambtha: l2 reg param
         returns: output of new layer
     """
+    kernel_r = tf.contrib.layers.l2_regularizer(lambtha)
+    kernel_i = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
     newL = tf.layers.Dense(n,
                            activation=activation,
-                           kernel_regularizer=tf.contrib.layers.l2_regularizer(lambtha),
-                           kernel_initializer=tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG"))(prev)
+                           kernel_regularizer=kernel_r,
+                           kernel_initializer=kernel_i)(prev)
     return newL
