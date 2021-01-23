83a91646bdda258dd40f49a6ab7459221e4b172b
mag389 <mag389@cornell.edu>
2021-01-23 05:58:15 +0000

    code cleanup

--- supervised_learning/0x05-regularization/7-early_stopping.py
+++ supervised_learning/0x05-regularization/7-early_stopping.py
@@ -15,6 +15,8 @@ def early_stopping(cost, opt_cost, threshold, patience, count):
        return: Bool of if it should be stopped, and new count
     """
     """
+    this as the basics of the when to stop early paper
+    but not the mehtod we are using in this assignment
     if (cost / opt_cost - 1) > threshold:
         return True, count
     pk = ( cost / count / opt_cost) - 1
@@ -29,10 +31,6 @@ def early_stopping(cost, opt_cost, threshold, patience, count):
         return True, count
     return false, count
     """
-    # if count > patience:
-    #     return False, count
-    # if cost < opt_cost:
-    #     count = 0
     if cost < opt_cost - threshold:
         return False, 0
     if count >= patience - 1:
