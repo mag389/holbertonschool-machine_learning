9d3223203737474c1d082de7e5a3c698a078f3b0
mag389 <mag389@cornell.edu>
2021-01-26 19:42:15 +0000

    save and laod weigts functions

---
+++ supervised_learning/0x06-keras/10-weights.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" saveing and loading wieghts """
+import tensorflow.keras as K
+
+
+def save_weights(network, filename, save_format='h5'):
+    """ saves the weights from network in file filename, in format given """
+    # weights = K.layers.get_weights()
+    network.save_weights(filename, save_format=save_format)
+    return None
+
+
+def load_weights(network, filename):
+    """ loads weights from a file to a netowrk"""
+    network.load_weights(filename)
+    return Noe
