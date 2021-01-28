43dc9aedd5198ad196a5fa6ebab6d05d0aa94dc0
mag389 <mag389@cornell.edu>
2021-01-28 03:03:15 +0000

    basic convolution

---
+++ math/0x04-convolution_and_pooling/0-convolve_grayscale_valid.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+""" performs convolution on grayscale images """
+import numpy as np
+from math import ceil, floor
+
+
+def convolve_grayscale_valid(images, kernel):
+    """ performs convolution on images based on kernel
+        images: np.ndarray (m, h, w)
+            m: number of images, h: height in pixels, w: width in pixels
+        kernel: np.ndarray (kh, kw) kernel for convolution
+            kh: height of kernel, kw: width of kernel
+    Returns: np.ndarray of convolved image
+    """
+    imgshape = images.shape
+    h = images.shape[1]
+    w = images.shape[2]
+    kh = kernel.shape[0]
+    kw = kernel.shape[1]
+    conved = np.zeros((imgshape[0], h - kh + 1, w - kw + 1))
+    wcounter = 0
+    hcounter = 0
+    # print(conved.shape)
+    # print(kernel.shape, images.shape)
+    # print(kernel[None, :, :].shape)
+    for i in range(0, h - kh):
+        for j in range(0, w - kw):
+            subs = images[:, i:i + kh, j:j + kw]
+            conved[:, i, j] = np.sum((kernel[None, :, :] * subs),
+                                     axis=(1, 2))
+
+    return conved
---
+++ math/0x04-convolution_and_pooling/README.md
@@ -0,0 +1,6 @@
+# Convolution and pooling
+
+The  math and basic implementtion of convolution and pooling
+
+Convolution and pooling are techniques use aid in image processing 
+in machine learning
