47063009e741f3849223ff7bfc22f8901cd55f38
mag389 <mag389@cornell.edu>
2021-01-28 03:18:03 +0000

    convolution with same size padding

---
+++ math/0x04-convolution_and_pooling/1-convolve_grayscale_same.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" performs convolution on grayscale images with potential pad"""
+import numpy as np
+from math import ceil, floor
+
+
+def convolve_grayscale_same(images, kernel):
+    """ performs convolution on images based on kernel
+        images: np.ndarray (m, h, w)
+            m: number of images, h: height in pixels, w: width in pixels
+        kernel: np.ndarray (kh, kw) kernel for convolution
+            kh: height of kernel, kw: width of kernel
+    Returns: np.ndarray of convolved image
+    """
+    imgshape = images.shape
+    h = images.shape[1]
+    w = images.shape[2]
+    kh = kernel.shape[0]
+    kw = kernel.shape[1]
+    # conved = np.zeros((imgshape[0], h - kh + 1, w - kw + 1))
+    conved = np.zeros(imgshape)
+    ipad = int((kh - 1) / 2)
+    jpad = int((kw - 1) / 2)
+    # print(conved.shape)
+    # print(kernel.shape, images.shape)
+    # print(kernel[None, :, :].shape)
+    for i in range(0, h - kh):
+        for j in range(0, w - kw):
+            subs = images[:, i:i + kh, j:j + kw]
+            ip = i + ipad
+            jp = j + jpad
+            conved[:, ip, jp] = np.sum((kernel[None, :, :] * subs),
+                                       axis=(1, 2))
+
+    return conved
