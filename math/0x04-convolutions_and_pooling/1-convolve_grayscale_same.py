a78692bc80bdb5825d1f716b7a7074d4f3e96af2
mag389 <mag389@cornell.edu>
2021-01-29 02:44:55 +0000

    wrong loop bounds again

--- math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/0x04-convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -23,8 +23,8 @@ def convolve_grayscale_same(images, kernel):
     # print(conved.shape)
     # print(kernel.shape, images.shape)
     # print(kernel[None, :, :].shape)
-    for i in range(0, h - kh):
-        for j in range(0, w - kw):
+    for i in range(0, h - kh + 1):
+        for j in range(0, w - kw + 1):
             subs = images[:, i:i + kh, j:j + kw]
             ip = i + ipad
             jp = j + jpad
