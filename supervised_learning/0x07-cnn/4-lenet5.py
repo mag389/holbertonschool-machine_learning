4ca663bc4b2ce0ac45ff9d22d913c62db5a91e01
mag389 <mag389@cornell.edu>
2021-02-04 21:07:23 +0000

    lenet draft file

---
+++ supervised_learning/0x07-cnn/4-lenet5.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+""" builds modified lenet5 with tensorflow without keras """
+import tensorflow as tf
+
+
+def lenet5(x, y):
+    """ builds modified lenet5
+        x: tf.placeholder (m, 28, 28, 1) input images (m is number of images)
+        y: tf.placeholder (m, 10) one-hot labels
+        in order layers:
+            l1: conv layer, 6 kernels (5x5) same padding
+            l2: max pool layer 2x2 kernel with 2,2 strides
+            l3: conv layer 16 kernels (5x5) valid padding
+            l4: max pool layer 3x3 kernel with 2,2 strides
+            l5: fully connected layer, 120 nodes
+            l6: fully connected, layer 84 nodes
+            l7: fully connected softmax output layer, 10 nodes
+        hidden layers use ReLu
+        Returns: activated output tensor, adam training op, loss tensor
+          and accurcy tensor
+    """
+    init = tf.contrib.layers.variance_scaling_initializer()
+    
+
+    l1 = tf.layers.Conv2D(6, (5, 5), padding='same', activation=tf.nn.relu,
+                          kernel_initializer=init)(x)                 
+    l2 = tf.layers.MaxPooling2D((2, 2), (2, 2))(l1)
+    l3 = tf.layers.Conv2D(16, (5, 5), padding='valid', activation=tf.nn.relu,
+                          kernel_initializer=init)(l2)
+    l4 = tf.layers.MaxPooling2D((3, 3), (2, 2))(l3)
+    flat = tf.layers.Flatten()(l4)
+    l5 = tf.layers.Dense(120, tf.nn.relu,
+                         kernel_initializer=init)(flat)
+    l6 = tf.layers.Dense(84, tf.nn.relu,
+                         kernel_initializer=init)(l5)
+    l7 = tf.layers.Dense(10,
+                         kernel_initializer=init)(l6)
+    pred = tf.nn.softmax(l7)
+    train = tf.train.AdamOptimizer(y, l7)
+    loss = tf.losses.softmax_cross_entropy(y, l7)
+    diff = tf.equal(tf.argmax(y, 1), tf.argmax(l7, 1))
+    acc = tf.reduce_mean(tf.cast(diff, tf.float32))
+
+    return pred, train, loss, acc
