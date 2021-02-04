11d2c9ec8595b63353929e0d0f25ab30d63a890e
mag389 <mag389@cornell.edu>
2021-02-04 22:17:55 +0000

    adjusting return calculations

--- supervised_learning/0x07-cnn/4-lenet5.py
+++ supervised_learning/0x07-cnn/4-lenet5.py
@@ -1,59 +1,57 @@
 #!/usr/bin/env python3
-"""Convolutional Neural Networks"""
+""" builds modified lenet5 with tensorflow without keras """
 import tensorflow as tf
 
 
 def lenet5(x, y):
-    """LeNet-5 architecture using tf. 3D image, RGB image - color
-    Arg:
-       x: tf.placeholder of shape (m, 28, 28, 1)
-          containing the input images
-       y: f.placeholder of shape (m, 10)
-          containing the one-hot labels
-    Return:
-        a tensor for the softmax activated output
-        a training operation that utilizes Adam optimization
-        a tensor for the loss of the netowrk
-        a tensor for the accuracy of the network
+    """ builds modified lenet5
+        x: tf.placeholder (m, 28, 28, 1) input images (m is number of images)
+        y: tf.placeholder (m, 10) one-hot labels
+        in order layers:
+            l1: conv layer, 6 kernels (5x5) same padding
+            l2: max pool layer 2x2 kernel with 2,2 strides
+            l3: conv layer 16 kernels (5x5) valid padding
+            l4: max pool layer 3x3 kernel with 2,2 strides
+            l5: fully connected layer, 120 nodes
+            l6: fully connected, layer 84 nodes
+            l7: fully connected softmax output layer, 10 nodes
+        hidden layers use ReLu
+        Returns: activated output tensor, adam training op, loss tensor
+          and accurcy tensor
     """
-    k_init = tf.contrib.layers.variance_scaling_initializer()
-    activation = tf.nn.relu
+    init = tf.contrib.layers.variance_scaling_initializer()
 
-    layer_1 = tf.layers.Conv2D(filters=6, kernel_size=5,
-                               padding='same',
-                               activation=activation,
-                               kernel_initializer=k_init)(x)
+    l1 = tf.layers.Conv2D(filters=6, kernel_size=(5, 5),
+                          padding='same', activation=tf.nn.relu,
+                          kernel_initializer=init)(x)
 
-    pool_1 = tf.layers.MaxPooling2D(pool_size=[2, 2],
-                                    strides=2)(layer_1)
+    l2 = tf.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(l1)
 
-    layer_2 = tf.layers.Conv2D(filters=16, kernel_size=5,
-                               padding='valid',
-                               activation=activation,
-                               kernel_initializer=k_init)(pool_1)
+    l3 = tf.layers.Conv2D(filters=16, kernel_size=(5, 5),
+                          padding='valid', activation=tf.nn.relu,
+                          kernel_initializer=init)(l2)
 
-    pool_2 = tf.layers.MaxPooling2D(pool_size=[2, 2],
-                                    strides=2)(layer_2)
+    l4 = tf.layers.MaxPooling2D(pool_size=(3, 3), strides=(2, 2))(l3)
 
-    flatten = tf.layers.Flatten()(pool_2)
+    flat = tf.layers.Flatten()(l4)
 
-    layer_3 = tf.layers.Dense(units=120, activation=activation,
-                              kernel_initializer=k_init)(flatten)
+    l5 = tf.layers.Dense(units=120, activation=tf.nn.relu,
+                         kernel_initializer=init)(flat)
 
-    layer_4 = tf.layers.Dense(units=84, activation=activation,
-                              kernel_initializer=k_init)(layer_3)
+    l6 = tf.layers.Dense(units=84, activation=tf.nn.relu,
+                         kernel_initializer=init)(l5)
 
-    output_layer = tf.layers.Dense(units=10,
-                                   kernel_initializer=k_init)(layer_4)
+    l7 = tf.layers.Dense(units=10,
+                         kernel_initializer=init)(l6)
 
-    y_pred = tf.nn.softmax(output_layer)
+    y_pred = tf.nn.softmax(l7)
 
-    loss = tf.losses.softmax_cross_entropy(y, output_layer)
+    loss = tf.losses.softmax_cross_entropy(y, l7)
 
     train = tf.train.AdamOptimizer().minimize(loss)
 
     equality = tf.equal(tf.argmax(y, axis=1),
-                        tf.argmax(output_layer, axis=1))
-    acc = tf.reduce_mean(tf.cast(equality, tf.float32))
+                        tf.argmax(l7, axis=1))
+    acc = tf.reduce_mean(tf.cast(diff, tf.float32))
 
     return y_pred, train, loss, acc
