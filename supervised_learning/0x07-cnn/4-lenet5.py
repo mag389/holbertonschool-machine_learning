2ae880f89d36abaf365c9beffea91a77cf4c5126
mag389 <mag389@cornell.edu>
2021-02-04 22:40:20 +0000

    changing names of things to try to find error

--- supervised_learning/0x07-cnn/4-lenet5.py
+++ supervised_learning/0x07-cnn/4-lenet5.py
@@ -19,8 +19,8 @@ def lenet5(x, y):
         Returns: activated output tensor, adam training op, loss tensor
           and accurcy tensor
     """
-    kinit = tf.contrib.layers.variance_scaling_initializer()
-    acti = tf.nn.relu
+    k_init = tf.contrib.layers.variance_scaling_initializer()
+    activation = tf.nn.relu
 
     # l1 = tf.layers.Conv2D(filters=6, kernel_size=(5, 5),
     #                      padding='same',
@@ -28,38 +28,38 @@ def lenet5(x, y):
     #                      kernel_initializer=kinit)(x)
     layer_1 = tf.layers.Conv2D(filters=6, kernel_size=5,
                                padding='same',
-                               activation=acti,
-                               kernel_initializer=kinit)(x)
+                               activation=activation,
+                               kernel_initializer=k_init)(x)
 
     pool2 = tf.layers.MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(layer_1)
 
     l3 = tf.layers.Conv2D(filters=16, kernel_size=(5, 5),
                           padding='valid',
-                          activation=acti,
-                          kernel_initializer=kinit)(pool2)
+                          activation=activation,
+                          kernel_initializer=k_init)(pool2)
 
     pool4 = tf.layers.MaxPooling2D(pool_size=(3, 3),
                                    strides=(2, 2))(l3)
 
-    flat = tf.layers.Flatten()(pool4)
+    flatten = tf.layers.Flatten()(pool4)
 
-    l5 = tf.layers.Dense(units=120, activation=acti,
-                         kernel_initializer=kinit)(flat)
+    l5 = tf.layers.Dense(units=120, activation=activation,
+                         kernel_initializer=k_init)(flatten)
 
-    l6 = tf.layers.Dense(units=84, activation=acti,
-                         kernel_initializer=kinit)(l5)
+    l6 = tf.layers.Dense(units=84, activation=activation,
+                         kernel_initializer=k_init)(l5)
 
-    l7 = tf.layers.Dense(units=10,
-                         kernel_initializer=kinit)(l6)
+    output_layer = tf.layers.Dense(units=10,
+                                   kernel_initializer=k_init)(l6)
 
-    y_pred = tf.nn.softmax(l7)
+    y_pred = tf.nn.softmax(output_layer)
 
-    loss = tf.losses.softmax_cross_entropy(y, l7)
+    loss = tf.losses.softmax_cross_entropy(y, output_layer)
 
     train = tf.train.AdamOptimizer().minimize(loss)
 
     equality = tf.equal(tf.argmax(y, axis=1),
-                        tf.argmax(l7, axis=1))
+                        tf.argmax(output_layer, axis=1))
     acc = tf.reduce_mean(tf.cast(equality, tf.float32))
 
     return y_pred, train, loss, acc
