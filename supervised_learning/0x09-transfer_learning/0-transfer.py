f83c5c9bce8cef8fac410709d2a2da0842d3e0f8
Michael <mag389@cornell.edu>
2021-02-18 20:39:16 +0000

    a working script, although not tested with main yet

--- supervised_learning/0x09-transfer_learning/0-transfer.py
+++ supervised_learning/0x09-transfer_learning/0-transfer.py
@@ -15,37 +15,52 @@ def preprocess_data(X, Y):
     x_r = K.applications.densenet.preprocess_input(X)
     # data must be preprocessed differently for different CNN's
     # can replace the x_r function if using a different neural network arch
-    """ this does preprocessing, but for receiving streams of img data
-    datagen = ImageDataGenerator(
-        featurewise_center=True,
-        featurewise_std_normalization=True,
-        rotation_range=20,
-        width_shift_range=0.2,
-        height_shift_range=0.2,
-        horizontal_flip=True)
-    """
     return x_r, y_r
 
 
 if __name__ == "__main__":
-    dense121 = K.applications.DenseNet121
+    dense121 = K.applications.densenet.DenseNet121
 
     (x, y), (xv, yv) = K.datasets.cifar10.load_data()
     x_train, y_train = preprocess_data(x, y)
     x_test, y_test = preprocess_data(xv, yv)
+    print(x_train.shape)
 
     classes = 10
     model = dense121(
         weights="imagenet",
         include_top=False,
-        input_shape=(32, 32, 3),
-        classes=10)
+        pooling='max',
+        input_shape=(32, 32, 3))
 
-    model.summary()
+    # model.summary()
     # K.utils.plot_model(model, to_file="model.png")
-    model.save("cifar10.h5")
-    # return model
+
+    # freeze model
+    model.trainable = False
+
+    # add new layers
+    newl = K.layers.Dense(256, activation='relu')(model.output)
+    newl = K.layers.Dropout(0.3)(newl)
+    newl = K.layers.Dense(10, activation='softmax')(newl)
+    new_model = K.Model(inputs=model.inputs, outputs=newl)
+
+    new_model.summary()
+
+    # runs slowly so adding checkpoints
+    calls = []
+    calls.append(K.callbacks.ModelCheckpoint("cifar10.h5",
+                 save_best_only=True))
+
+    # compile the model
+    new_model.compile(
+        optimizer='adam',
+        loss='categorical_crossentropy',
+        metrics=['acc'])
+    # train
+    new_model.fit(x_train, y_train, batch_size=256, epochs=1,
+                  verbose=True,
+                  callbacks=calls,
+                  shuffle=False)
+    new_model.save("cifar10.h5")
     # model.save('cifar10.h5')
-
-
-
