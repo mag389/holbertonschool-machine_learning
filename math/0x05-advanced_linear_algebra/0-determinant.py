c5a509fcaad6bffbc3480689d5d1bb70ed49d78a
mag389 <mag389@cornell.edu>
2021-03-17 20:16:29 +0000

    removed round again

--- math/0x05-advanced_linear_algebra/0-determinant.py
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -15,18 +15,16 @@ def determinant(matrix):
             raise TypeError("matrix must be a list of lists")
     if len(matrix) != len(matrix[0]):
         raise ValueError("matrix must be a square matrix")
-    """
     for row in matrix:
         if len(row) != size:
             raise TypeError("matrix must be a square matrix")
-    """
 
     if size == 1:
         return matrix[0][0]
     if size == 2:
         return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
     num = deter(matrix)
-    return round(num)
+    return num
 
 
 def deter(matrix):
