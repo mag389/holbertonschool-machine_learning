9c508d46d718abddfee8a196b2517f62befb6412
mag389 <mag389@cornell.edu>
2021-03-17 20:57:59 +0000

    minor matrix creation

--- math/0x05-advanced_linear_algebra/1-minor.py
+++ math/0x05-advanced_linear_algebra/1-minor.py
@@ -52,50 +52,43 @@ def determinant(matrix):
     """ calculate determinant of matrix """
     if matrix == [[]]:
         return 1
-    if type(matrix) is not list or len(matrix) < 1:
-        raise TypeError("matrix must be a list of lists")
-    size = len(matrix)
-    # print(size)
-    for row in matrix:
-        if type(row) is not list:
+
+    shape_col = [len(row) for row in matrix]
+    if (isinstance(matrix, list)) and len(matrix) is not 0:
+        if not all(isinstance(row, list) for row in matrix):
             raise TypeError("matrix must be a list of lists")
-        # print(len(row))
-        if len(row) != size:
-            raise TypeError("matrix must be a square matrix")
-    if size == 1:
+    else:
+        raise TypeError("matrix must be a list of lists")
+    if not all(len(matrix) == col for col in shape_col):
+        raise ValueError("matrix must be a square matrix")
+
+    if len(matrix) == 1 and len(matrix[0]) == 1:
         return matrix[0][0]
-    num = deter(matrix)
-    if num >= 0:
-        return int(num + .5)
-    return int(num - 0.5)
+    return deter(matrix)
 
 
 def deter(matrix):
-    """ uses gaussian elimination steps """
+    """ uses elimination without recursion """
     size = len(matrix)
     coeff = 1
-    if matrix[0][0] == 0:
-        for i in range(1, size):
-            if matrix[i][0] != 0:
-                matrix[i], matrix[0] = matrix[0], matrix[i]
-                return -1 * deter(matrix)
-        return 0
-    else:
-        if size == 1:
-            return matrix[0][0]
-        coeff /= 1 / matrix[0][0]
-        matrix[0] = [matrix[0][x] / matrix[0][0] for x in range(size)]
-        # print("cur matr")
-        # print(matrix)
-        for i in range(1, size):
-            old = matrix[i][0]
-            for j in range(size):
-                # old = matrix[i][0]
-                # print("old is: %i", old)
-                matrix[i][j] = matrix[i][j] - matrix[0][j] * old
-        # print(matrix)
-        slicer = [matrix[x][1:] for x in range(1, len(matrix))]
-        # print(slicer)
-        # print(coeff)
-        # print("--------------")
-        return (coeff * deter(slicer))
+    for i in range(size):
+        if matrix[i][i] == 0:
+            switch = 0
+            for j in range(i + 1, size):
+                if matrix[j][i] != 0:
+                    matrix[j], matrix[i] = matrix[i], matrix[j]
+                    coeff *= -1
+                    switch = 1
+                    break
+            if switch == 0:
+                return 0
+        diag = matrix[i][i]
+        if diag == 0:
+            return 0
+        coeff *= matrix[i][i]
+        matrix[i] = [matrix[i][x] / diag for x in range(size)]
+        for j in range(i + 1, size):
+            old = matrix[j][i]
+            for k in range(size):
+                matrix[j][k] = matrix[j][k] - matrix[i][k] * old
+    return round(coeff)
