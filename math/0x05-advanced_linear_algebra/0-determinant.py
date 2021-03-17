d3bb1a9aec569d66ff89b6cde1b64459d80fb475
mag389 <mag389@cornell.edu>
2021-03-17 20:07:05 +0000

    using the bad method instead of the reduction method

--- math/0x05-advanced_linear_algebra/0-determinant.py
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -12,11 +12,11 @@ def determinant(matrix):
     for row in matrix:
         if type(row) is not list:
             raise TypeError("matrix must be a list of lists")
+    for row in matrix:
         if len(row) != size:
             raise TypeError("matrix must be a square matrix")
     if size == 1:
         return matrix[0][0]
-
     if size == 2:
         return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
     num = deter(matrix)
@@ -24,35 +24,43 @@ def determinant(matrix):
 
 
 def deter(matrix):
+    """ get determinant by using sub determinants
+        very slow for lager matrices
+    """
+    size = len(matrix)
+    if size == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
+    sign = 1
+    det = 0
+    for idx in range(size):
+        copy = [row.copy() for row in matrix]
+        copy = copy[1:]
+        new_size = len(copy)
+
+        for i in range(new_size):
+            copy[i] = copy[i][0:idx] + copy[i][idx + 1:]
+        sub = deter(copy)
+        det += sign * matrix[0][idx] * sub
+        sign *= -1
+    return det
+
+
+def deterreduce(matrix):
     """ uses elimination without recursion """
     size = len(matrix)
     coeff = 1
-    """
-    for i in range(size):
-        if matrix[i][i] == 0:
-            for j in range(i + 1, size):
-                if matrix[j][i] != 0:
-                    matrix[j], matrix[i] = matrix[i], matrix[j]
-                    coeff *= -1
-                    break
-                return 0
-    """
-    # for i in range(size):
-    #     print(matrix[i])
     for i in range(size):
         if matrix[i][i] == 0:
             switch = 0
             for j in range(i + 1, size):
                 if matrix[j][i] != 0:
                     matrix[j], matrix[i] = matrix[i], matrix[j]
-                    # print(matrix)
                     coeff *= -1
                     switch = 1
                     break
             if switch == 0:
                 return 0
-        # print(matrix)
-        # print("-------")
         diag = matrix[i][i]
         if diag == 0:
             return 0
@@ -66,7 +74,7 @@ def deter(matrix):
 
 
 def deter1(matrix):
-    """ uses gaussian elimination steps """
+    """ deprecated mathod produces same error as above """
     size = len(matrix)
     coeff = 1
     if matrix[0][0] == 0:
