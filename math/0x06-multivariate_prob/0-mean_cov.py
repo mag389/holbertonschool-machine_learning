447c846d30cb0cbd7e1ca718befa1f1fd2bbcfb9
mag389 <mag389@cornell.edu>
2021-03-23 03:39:10 +0000

    initial commit

---
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+""" mean and cov of data set """
+import numpy as np
+
+
+def mean_cov(X):
+    """ calcs mean and covariance of a data set
+        X: np ndarray (n, d)
+          n: number of points, d: number of dimensions of each data point
+        Returns: mean, cov
+    """
+    if len(X.shape) != 2 or type(X) != np.ndarray:
+        raise TypeError("X must be a 2D numpy.ndarray")
+    if X.shape[0] < 2:
+        raise ValueError("X must contain multiple data points")
+    n = X.shape[0]
+    d = X.shape[1]
+    mean = np.mean(X, axis=0, keepdims=True)
+    cov = (1 / (n - 1)) * np.matmul(X.T - mean.T, X - mean)
+    return (mean, cov)
