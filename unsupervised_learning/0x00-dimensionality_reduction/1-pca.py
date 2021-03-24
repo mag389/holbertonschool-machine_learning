67fb4528ba0005a78d160f3b2554fcce471959aa
mag389 <mag389@cornell.edu>
2021-03-24 18:54:46 +0000

    pca with ndims

---
+++ unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" pca svd implementation """
+import numpy as np
+
+
+def pca(X, ndim):
+    """ perform pca on X
+        X: np.ndarray (n, d)
+          n: number of data points
+          d: number of dimensions in each point
+          all dimension have mean of 0 across all data points
+        ndim: new dimensionality of transformed X
+        Returns: matrix T of transformation of X
+          T: (d, ndim) nd is new dimensionality
+        @ is matmul
+    """
+    u, s, vh = np.linalg.svd(X)
+    # traditional notation decomp(A) = U (sigma) VT = (u * s) @ vh
+    W = vh[0:ndim].T
+    X_m = X - np.mean(X, axis=0)
+    return np.matmul(X_m, W)
--- unsupervised_learning/0x00-dimensionality_reduction/README.md
+++ unsupervised_learning/0x00-dimensionality_reduction/README.md
@@ -1,2 +1,16 @@
 # dimensionality reduction
+
+
 pricipal component analysis and single value decomposition
+
+data:
+curl -o mnist2500_labels.txt 'https://holbertonintranet.s3.amazonaws.com
+/uploads/text/2019/10/72a86270e2a1c2cbc14b.txt?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Crede
+ntial=AKIARDDGGGOUWMNL5ANN%2F20210324%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20210324T1
+61801Z&X-Amz-Expires=345600&X-Amz-SignedHeaders=host&X-Amz-Signature=0b53e44fc08f7f86bea6ba
+2e8bbc428bfb512f9ce2f53b7d3c4af1a3f262d9f1'
+
+curl -o mnist2500_X.txt 'https://intranet-projects-files.s3.amazonaws.co
+m/holbertonschool-ml/mnist2500_X.txt'
+
+i believe the first was too large for github
