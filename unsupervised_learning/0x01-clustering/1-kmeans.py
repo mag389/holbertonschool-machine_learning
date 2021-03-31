27bc01f76d30f47b674d37744b56e4bfa09a4a9a
mag389 <mag389@cornell.edu>
2021-03-31 18:45:49 +0000

    len instead of .size)

--- unsupervised_learning/0x01-clustering/1-kmeans.py
+++ unsupervised_learning/0x01-clustering/1-kmeans.py
@@ -27,12 +27,14 @@ def kmeans(X, k, iterations=1000):
     if len(X.shape) != 2:
         return None, None
     n, d = X.shape
+
     # init centroids and clss
     rmax = np.amax(X, axis=0)
     rmin = np.amin(X, axis=0)
     C = np.random.uniform(rmin, rmax, (k, d))
     clss = np.zeros(n)
     change = 0
+
     # perform clustering iterations
     for i in range(iterations):
         C_cpy = np.copy(C)
@@ -43,7 +45,7 @@ def kmeans(X, k, iterations=1000):
         clss = np.argmin(distances, axis=0)
         for j in range(k):
             # check no class is empty
-            if X[clss == j].size == 0:
+            if len(X[clss == j]) == 0:
                 C[j] = np.random.uniform(rmin, rmax, (1, d))
             else:
                 # recalc centroid
