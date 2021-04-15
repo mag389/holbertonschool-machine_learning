e3268aa6b29e07dd8c9b487086978465c2541961
mag389 <mag389@cornell.edu>
2021-04-15 20:08:50 +0000

    trying variation of my original method

--- unsupervised_learning/0x03-hyperparameter_tuning/4-bayes_opt.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/4-bayes_opt.py
@@ -37,6 +37,22 @@ class BayesianOptimization():
               EI: np arr (ac_samples,) of expected improvement of each
                 potential sample
         """
+        # trying slight variation
+        ml_sp, sg_spl = self.gp.predict(self.X_s)
+        if self.minimize is True:
+            value = np.min(self.gp.Y)
+            retour = value - ml_sp - self.xsi
+        else:
+            value = np.max(self.gp.Y)
+            retour = ml_sp - value - self.xsi
+        with np.errstate(divide='warn'):
+            Z = retour / sg_spl
+            EI = retour * norm.cdf(Z) + sg_spl * norm.pdf(Z)
+
+        X_next = self.X_s[np.argmax(EI)]
+
+        return X_next, EI
+        # end test
         mu, sigma = self.gp.predict(self.X_s)
         s = len(mu)
         mu_sample_opt = np.max(self.gp.Y)
@@ -47,7 +63,7 @@ class BayesianOptimization():
             imp = mu - mu_sample_opt - self.xsi
             Z = imp / sigma
             EI = imp * norm.cdf(Z) + sigma * norm.pdf(Z)
-            # EI[sigma == 0.0] = 0.0
+            EI[sigma == 0.0] = 0.0
         X_next = self.X_s[np.argmax(EI)]
 
         return X_next, EI
