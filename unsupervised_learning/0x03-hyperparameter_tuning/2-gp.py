a653e269954608e61eb1d174c4f0148672ffbcba
mag389 <mag389@cornell.edu>
2021-04-15 22:29:31 +0000

    comparing to another program entirely

--- unsupervised_learning/0x03-hyperparameter_tuning/2-gp.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/2-gp.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
-""" GaussianProcess file for representing gaussian process """
+""" task 0"""
+
+
 import numpy as np
 
 
 class GaussianProcess():
-    """ noiseless 1D gaussian process """
+    """ classe description """
+
     def __init__(self, X_init, Y_init, l=1, sigma_f=1):
-        """ initialize GaussianProcess object
-            X_init: np arr (t, 1) of inputs sampled with black-box f'n
-            Y_init: np arr (t, 1) of outputs of blackbox f'n for each input
-              t: number of initial samples
-            l: length parameter for kernel
-            sigma_f: standard dev given to output of black-box f'n
-            calculates K representing covariance matrix
+        """
+        constructeur
+        :param X_init:
+        :param Y_init:
+        :param l:
+        :param sigma_f:
         """
         self.X = X_init
         self.Y = Y_init
@@ -21,51 +23,40 @@ class GaussianProcess():
         self.K = self.kernel(X_init, X_init)
 
     def kernel(self, X1, X2):
-        """ calculates covariance kernel matrix between two matrices
-            X1, X2: np arrays (m, 1) and (n, 1) respectively
-            uses radial basis fucntion (RBF)
-            Returns: covariance kernel matrix np arr (m, n)
         """
-        sqdist = np.sum(X1**2, 1).reshape(-1, 1)\
-            + np.sum(X2**2, 1) - 2 * np.matmul(X1, X2.T)
-        return self.sigma_f**2 * np.exp(-0.5 / self.l**2 * sqdist)
+        calcule de covariance
+        :param X1:
+        :param X2:
+        """
+        somme = np.sum(X1 ** 2, 1).reshape(-1, 1) + np.sum(X2 ** 2, 1)
+        retrait = somme + (-2 * np.dot(X1, X2.T))
+        final = self.sigma_f ** 2 * np.exp(-0.5 / self.l ** 2 * retrait)
+        return final
 
     def predict(self, X_s):
-        """ predicts the mean and std dev of pts in gaussian process
-            X_s: np arr (s, 1) of all pts whose mean and std dev to calculate
-              s: number of sample points
-            Returns: mu, sigma
-              mu: np arr (s,) of mean for each pt in X_s
-              sigma: np arr (s,) of variance for each pt in X_s
         """
-        # making sure this is correct
-        # Krnl_ss = self.kernel(X_s, X_s)
-        # Krnl_inv = np.linalg.inv(self.K)
-        # Krnl_s = self.kernel(self.X, X_s)
-        # this line in mine was the error
-        # conv_s = Krnl_ss - Krnl_s.T.dot(Krnl_inv).dot(Krnl_s)
-        # sigma = np.diagonal(conv_s)
+        prediction de la moyenne
+        :param X_s:
+        """
+        Krnl_ss = self.kernel(X_s, X_s)
+        Krnl_inv = np.linalg.inv(self.K)
+        Krnl_s = self.kernel(self.X, X_s)
 
-        # multi = Krnl_s.T.dot(Krnl_inv).dot(self.Y)
-        # multi = np.reshape(multi, -1)
+        conv_s = Krnl_ss - Krnl_s.T.dot(Krnl_inv).dot(Krnl_s)
+        sigma = np.diagonal(conv_s)
 
-        # return multi, sigma
-        # end
-        s = X_s.shape[0]
-        K_s = self.kernel(self.X, X_s)
-        K_ss = self.kernel(X_s, X_s)  # + 1e-8 * np.eye(len(X_s))
-        K_inv = np.linalg.inv(self.K)
+        multi = Krnl_s.T.dot(Krnl_inv).dot(self.Y)
+        multi = np.reshape(multi, -1)
 
-        mu = K_s.T.dot(K_inv).dot(self.Y).reshape((s,))
-        sigma = K_ss - K_s.T.dot(K_inv).dot(K_s)
-
-        return mu, np.diag(sigma)
+        return multi, sigma
 
     def update(self, X_new, Y_new):
-        """ updates the gaussian object instance
-            X_new, Y_new: np arrays (1,) of new sample point and new sample f'n
-            updates X, Y, K
         """
-        self.X = np.append(self.X, X_new.reshape((1, 1)), 0)
-        self.Y = np.append(self.Y, Y_new.reshape((1, 1)), 0)
+        mise a jour
+        :param X_new:
+        :param Y_new:
+        """
+        self.X = np.concatenate((self.X, X_new[:, np.newaxis]), axis=0)
+        self.Y = np.append(self.Y, Y_new)
+        self.Y = np.reshape(self.Y, (-1, 1))
         self.K = self.kernel(self.X, self.X)
--- unsupervised_learning/0x03-hyperparameter_tuning/5-bayes_opt.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/5-bayes_opt.py
@@ -1,43 +1,41 @@
 #!/usr/bin/env python3
-""" bayes optimization """
+""" task 4 """
+
+
 import numpy as np
 from scipy.stats import norm
 GP = __import__('2-gp').GaussianProcess
 
 
 class BayesianOptimization():
-    """ 1d bayesaian gaussian process """
-    def __init__(self, f, X_init, Y_init, bounds, ac_samples, l=1, sigma_f=1,
-                 xsi=0.01, minimize=True):
-        """ initializer
-            f: black box function to be optimizd
-            X_init: np arr (t, 1) of inputs already sample with blackblox f'n
-            Y_init: np arr (t, 1) of outputs of BB f'n
-              t: number of initial samples
-            bounds: tuple of (min, max) of bounds of the space to look for
-              optimal point
-            ac_samples: number of samples to analyze during acquisition
-            l: length parameter for kernel
-            sigma_f: standard deviation given to output of black box f'n
-            xsi: oxploration-exploitation factor for acquisition
-            minimize: bool to perform minimization (true) or optimization
+    """ classe de performance de bayesian  """
+
+    def __init__(self, f, X_init, Y_init, bounds,
+                 ac_samples, l=1, sigma_f=1, xsi=0.01, minimize=True):
         """
+        constructeur parametré
+        :param f:
+        :param X_init:
+        :param Y_init:
+        :param bounds:
+        :param ac_samples:
+        :param l:
+        :param sigma_f:
+        :param xsi:
+        :param minimize:
+        """
+
         self.f = f
         self.gp = GP(X_init, Y_init, l, sigma_f)
-        ac = ac_samples
-        self.X_s = np.linspace(bounds[0], bounds[1], ac).reshape((ac, 1))
+        X_s = np.linspace(bounds[0], bounds[1], num=ac_samples, retstep=False)
+        self.X_s = X_s.reshape(-1, 1)
         self.xsi = xsi
         self.minimize = minimize
 
     def acquisition(self):
-        """ calculates next best sample location
-            Uses Expected improvement acquisition function
-            Returns: X_next, EI
-              X_next: np arr (1,) of next best sample point
-              EI: np arr (ac_samples,) of expected improvement of each
-                potential sample
         """
-        # comparison test for zero division
+        description
+        """
         ml_sp, sg_spl = self.gp.predict(self.X_s)
         if self.minimize is True:
             value = np.min(self.gp.Y)
@@ -52,51 +50,29 @@ class BayesianOptimization():
         X_next = self.X_s[np.argmax(EI)]
 
         return X_next, EI
-        # end
-        mu, sigma = self.gp.predict(self.X_s)
-        s = len(mu)
-        mu_sample_opt = np.max(self.gp.Y)
-        if self.minimize is True:
-            mu_sample_opt = np.min(self.gp.Y)
-            mu_sample_opt = 2 * mu - mu_sample_opt
-        with np.errstate(divide='warn'):
-            imp = mu - mu_sample_opt - self.xsi
-            Z = imp / sigma
-            EI = imp * norm.cdf(Z) + sigma * norm.pdf(Z)
-            EI[sigma == 0.0] = 0.0
-        X_next = self.X_s[np.argmax(EI)]
-
-        return X_next, EI
-        """ i was on the right track but didn't end up using this code
-            instead determined min vs max at different point
-        if self.minimize == False:
-            mu_max = np.max(self.gp.Y)
-        else:
-            mu_min = np.min(self.gp.Y)
-        """
 
     def optimize(self, iterations=100):
-        """ optimizes the black box funciton
-            iterations: max number of iterations to perform
-            if next proposed point has already been sample stop early
-            Returns: X-opt, Y_opt
-              X_opt: np arr (1,) the optimal point
-              Y_opt: np arr (1,) the optimal function value
         """
-        # comparison testing
-        # end
-        visited = []
+        black  box
+        :param iterations:
+        """
+
+        tab = []
 
         for i in range(iterations):
-            X_next, ei = self.acquisition()
-            if X_next in visited:
+            suiv = self.acquisition()[0]
+            if suiv in tab:
                 break
-            Y_next = self.f(X_next)
-            self.gp.update(X_next, Y_next)
-            visited.append(X_next)
+            suiv_Y = self.f(suiv)
+            self.gp.update(suiv, suiv_Y)
+            tab.append(suiv)
 
-        if self.minimize is True:
-            opt = np.argmin(self.gp.Y)
+        if self.minimize is False:
+            idx = np.argmax(self.gp.Y)
         else:
-            opt = np.argmax(self.gp.Y)
-        return self.gp.X[opt], self.gp.Y[opt]
+            idx = np.argmin(self.gp.Y)
+
+        X_opt = self.gp.X[idx]
+        Y_opt = self.gp.Y[idx]
+
+        return X_opt, Y_opt
