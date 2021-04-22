5a190923e794e3fe4cff177f7169fa6b70318fef
mag389 <mag389@cornell.edu>
2021-04-22 21:19:42 +0000

    goind back to -1 bound to see if it worked

--- unsupervised_learning/0x04-autoencoders/3-variational.py
+++ unsupervised_learning/0x04-autoencoders/3-variational.py
@@ -40,7 +40,7 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     decode = keras.layers.Dense(input_dims, activation='sigmoid')(x)
 
     decoder = keras.Model(dinputs, decode)
-    outputs = decoder(encoder(inputs)[2])
+    outputs = decoder(encoder(inputs)[-1])
     auto = keras.Model(inputs, outputs)
 
     def vae_loss(inputs, outputs):
