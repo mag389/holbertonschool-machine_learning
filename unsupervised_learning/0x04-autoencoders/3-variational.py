94600607053288e658291bc241e9cc4912594fc8
mag389 <mag389@cornell.edu>
2021-04-22 21:18:31 +0000

    changes in looping bounds for both loops

--- unsupervised_learning/0x04-autoencoders/3-variational.py
+++ unsupervised_learning/0x04-autoencoders/3-variational.py
@@ -17,44 +17,43 @@ def autoencoder(input_dims, hidden_layers, latent_dims):
     """
     inputs = keras.Input(shape=(input_dims,))
     h = keras.layers.Dense(hidden_layers[0], activation='relu')(inputs)
-    for i in range(1, len(hidden_layers)):
-        dims = hiddel_layers[i]
+    for dims in hidden_layers[1:]:
         h = keras.layers.Dense(dims, activation='relu')(h)
     z_mean = keras.layers.Dense(latent_dims)(h)
     z_log_sigma = keras.layers.Dense(latent_dims)(h)
 
     def sampling(args):
-        """ sampling function for vae
-            args: mean, logvar
-        """
+        """ samplig f'n for vae """
         z_mean, z_log_sigma = args
-        K1 = keras.backend.shape(z_mean)[0]
-        epsilon = keras.backend.random_normal(shape=(K1, latent_dims),
-                                              mean=0, stddev=1)
+        epsilon = keras.backend.random_normal(
+            shape=(keras.backend.shape(z_mean)[0], latent_dims),
+            mean=0, stddev=1)
         return z_mean + keras.backend.exp(z_log_sigma / 2) * epsilon
+
     z = keras.layers.Lambda(sampling)([z_mean, z_log_sigma])
     encoder = keras.Model(inputs, [z_mean, z_log_sigma, z])
 
-    dinput = keras.Input(shape=(latent_dims,))
-    dh = keras.layers.Dense(hidden_layers[-1], activation='relu')(dinput)
-    for i in range(len(hidden_layers) - 2, 0, -1):
-        dims = hidden_layers[i]
-        dh = keras.layers.Dense(dims, activation='relu')(dh)
-    decode = keras.layers.Dense(input_dims, activation='sigmoid')(dh)
+    dinputs = keras.Input(shape=(latent_dims,))
+    x = keras.layers.Dense(hidden_layers[-1], activation='relu')(dinputs)
+    for dims in hidden_layers[-2::-1]:
+        x = keras.layers.Dense(dims, activation='relu')(x)
+    decode = keras.layers.Dense(input_dims, activation='sigmoid')(x)
 
-    decoder = keras.Model(dinput, decode)
-    outputs = decoder(encoder(inputs)[-1])
+    decoder = keras.Model(dinputs, decode)
+    outputs = decoder(encoder(inputs)[2])
     auto = keras.Model(inputs, outputs)
- 
-    def vae_loss(true, pred):
-        """ a separate custom loss fucntion """
-        rloss = keras.losses.binary_crossentropy(inputs, outputs)
-        rloss *= input_dims
+
+    def vae_loss(inputs, outputs):
+        """ separate custom loss function """
+        r_loss = keras.losses.binary_crossentropy(inputs, outputs)
+        r_loss *= input_dims
         kl_loss = 1 + z_log_sigma - keras.backend.square(z_mean) \
             - keras.backend.exp(z_log_sigma)
         kl_loss = keras.backend.sum(kl_loss, axis=-1)
         kl_loss *= -0.5
-        return keras.backend.mean(rloss + kl_loss)
+        vae_loss = keras.backend.mean(r_loss + kl_loss)
+        return vae_loss
+
     auto.compile(optimizer='adam', loss=vae_loss)
 
     return encoder, decoder, auto
