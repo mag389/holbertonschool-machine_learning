ba40efd127a43dba4b362f7c023bc6b65f7b433d
Michael George <mag389@cornell.edu>
2021-06-10 17:14:30 +0000

    adapt to work in colab

--- supervised_learning/0x13-qa_bot/1-loop.py
+++ supervised_learning/0x13-qa_bot/1-loop.py
@@ -10,5 +10,7 @@ if __name__ == "__main__":
         inp = input()
         if inp.lower() in words:
             print("A: Goodbye")
+            exited = 1
             exit()
-        print("A: ")
+        else:
+            print("A: ")
