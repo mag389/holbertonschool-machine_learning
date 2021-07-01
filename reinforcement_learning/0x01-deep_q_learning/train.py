4a41f3c887c352d18c096625e0cb18b638b2e580
mag389 <mag389@cornell.edu>
2021-07-01 17:33:30 +0000

    train file

---
+++ reinforcement_learning/0x01-deep_q_learning/train.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" training depp q net for atari breakout """
+import gym
+print("gym imported")
+import tensorflow.keras as k
+print("k imported")
+import tensorflow as tf
+print("tf imported")
+print(tf.__version__)
