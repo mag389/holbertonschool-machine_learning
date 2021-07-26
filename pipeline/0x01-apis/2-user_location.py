280bb9b941a2a5409f6387f1c4a5823279b7e192
mag389 <mag389@cornell.edu>
2021-07-26 18:33:33 +0000

    alt method testing

--- pipeline/0x01-apis/2-user_location.py
+++ pipeline/0x01-apis/2-user_location.py
@@ -1,22 +1,27 @@
 #!/usr/bin/env python3
-""" prints location of user """
+"""
+    Barely an API project.
+"""
 import requests
 import sys
 import time
 
 
-if __name__ == '__main__':
-    url = sys.argv[1]
-    res = requests.get(url)
-    if res.status_code == 403:
-        rate_lim = res.headers['X-Ratelimit-Reset']
-        res_time = int(reset) - int(time.time())
-        print("Reset in {} min".format(res_time))
-    elif res.status_code == 200:
-        loc = res.json()['location']
-        if loc is not None:
-            print(loc)
-        else:
-            print("Not found")
+if __name__ == "__main__":
+    """ prints location of user specified as cli arg """
+    user_url = sys.argv[1]
+    r = requests.get(user_url)
+    if r.status_code != 200:
+        if r.status_code == 403:
+            reset_time = int(r.headers.get('X-Ratelimit-Reset'))
+            now = time.time()
+            minutes = reset_time - now
+            minutes = round(minutes / 60)
+            print("Reset in {} min".format(minutes))
+            exit()
+    user = r.json()
+    location = user.get('location')
+    if location:
+        print(user.get('location'))
     else:
         print("Not found")
