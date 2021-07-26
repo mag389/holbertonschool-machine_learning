941b44c85740e80f8563b4ef1465611a0bfb0778
mag389 <mag389@cornell.edu>
2021-07-26 17:49:17 +0000

    check for blank location

--- pipeline/0x01-apis/2-user_location.py
+++ pipeline/0x01-apis/2-user_location.py
@@ -9,7 +9,11 @@ if __name__ == '__main__':
     url = sys.argv[1]
     res = requests.get(url)
     if res.status_code == 200:
-        print(res.json()['location'])
+        loc = res.json()['location']
+        if loc is not None:
+            print(loc)
+        else:
+            print("Not found")
     elif res.status_code == 403:
         rate_lim = res.headers['X-Ratelimit-Reset']
         res_time = int(reset) - int(time.time())
