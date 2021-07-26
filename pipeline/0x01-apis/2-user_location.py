508b4f3bd1818ebcd789b11c10bc93f03c36c96b
mag389 <mag389@cornell.edu>
2021-07-26 18:45:21 +0000

    alternate method take two

--- pipeline/0x01-apis/2-user_location.py
+++ pipeline/0x01-apis/2-user_location.py
@@ -5,28 +5,19 @@ import sys
 import time
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     url = sys.argv[1]
-    res = requests.get(url)
-    if res.status_code == 403:
-        rate_lim = res.headers['X-Ratelimit-Reset']
-        res_time = int(reset) - int(time.time())
-        print("Reset in {} min".format(res_time))
+    r = requests.get(url)
+    if r.status_code == 403:
+        reset_time = int(r.headers.get('X-Ratelimit-Reset'))
+        now = time.time()
+        minutes = reset_time - now
+        minutes = round(minutes / 60)
+        print("Reset in {} min".format(minutes))
         exit()
-    acc = res.json()
-    loc = acc.get('location')
-    if loc is not None:
-        print(loc)
-    else:
-        print(("Not found"))
-    exit()
-    """
-    elif res.status_code == 200:
-        loc = res.json()['location']
-        if loc is not None:
-            print(loc)
-        else:
-            print("Not found")
+    user = r.json()
+    location = user.get('location')
+    if location:
+        print(user.get('location'))
     else:
         print("Not found")
-    """
