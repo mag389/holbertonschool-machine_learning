daed65dd53ffbf2af45787a0542d5e7a206ea6e0
mag389 <mag389@cornell.edu>
2021-08-02 15:37:34 +0000

    country users table creation

---
+++ pipeline/0x02-databases/14-country_users.sql
@@ -0,0 +1,8 @@
+-- cretes a table users with requirements
+-- if exists won't fail
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') NOT NULL
+)
