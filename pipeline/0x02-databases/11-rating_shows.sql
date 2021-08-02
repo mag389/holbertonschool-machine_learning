ee2ff6eb0b518826178f1a1af17196936e744bb1
mag389 <mag389@cornell.edu>
2021-08-02 15:16:23 +0000

    lists shows by rating

---
+++ pipeline/0x02-databases/11-rating_shows.sql
@@ -0,0 +1,7 @@
+-- lists all show by rating
+SELECT title, SUM(tv_show_ratings.rate) AS rating 
+FROM tv_shows 
+JOIN tv_show_ratings
+ON id = tv_show_ratings.show_id
+GROUP BY title 
+ORDER BY rating DESC;
