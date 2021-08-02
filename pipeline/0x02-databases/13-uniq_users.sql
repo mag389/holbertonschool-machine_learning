c20138507629cd13e61faf91da2daf375fb5a478
mag389 <mag389@cornell.edu>
2021-08-02 15:35:17 +0000

    new table for users

---
+++ pipeline/0x02-databases/13-uniq_users.sql
@@ -0,0 +1,7 @@
+-- cretes a table users with requirements
+-- if exists won't fail
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255)
+)
