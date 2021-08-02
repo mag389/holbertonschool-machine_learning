0d149b741e51cce2275f5dd56eabe815439ef38b
mag389 <mag389@cornell.edu>
2021-08-02 16:09:02 +0000

    trigger for changing inventory on orders

---
+++ pipeline/0x02-databases/17-store.sql
@@ -0,0 +1,8 @@
+-- trigger to decrease quantity
+CREATE TRIGGER decrease_quantity
+AFTER INSERT
+ON orders
+FOR EACH ROW
+	UPDATE items
+	SET quantity = quantity - NEW.number
+	WHERE items.name = NEW.item_name;
