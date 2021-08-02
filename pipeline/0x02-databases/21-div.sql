37c41ad41f6ac3bb9baef00b5e3f815bf9a9e30b
mag389 <mag389@cornell.edu>
2021-08-02 18:43:57 +0000

    division function

---
+++ pipeline/0x02-databases/21-div.sql
@@ -0,0 +1,13 @@
+-- division function
+DELIMITER $$
+
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT
+	BEGIN
+		IF b = 0 THEN
+			RETURN 0;
+		END IF;
+		RETURN (a / b);
+	END $$
+
+DELIMITER ;
